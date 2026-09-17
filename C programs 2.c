e0331ab72ede344c0ac850df2732f2b00aded19a
joseph30617-design <joseph.30617@students.kyu.ac.ke>
2026-09-17 17:55:36 +0300

    Add files via upload

---
+++ C programs 2.c
@@ -0,0 +1,25 @@
+//program to display units of Bsc information technology  
+/*
+name :Joseph
+reg no:CT100/G/30617/26
+description :units of bachelor of information technology 
+date: 14/09/2026
+version 1
+*/
+#include <stdio.h> //scanf(),print("");int 
+
+int main ()
+{
+    printf("UNITS OF BSC IN INFORMATION TECHNOLOGY \n");
+    printf("1.maths for science\n");
+    printf("2.essential to accounting\n");
+    printf("3.introduction to computer application\n");
+    printf("4.computer ays and organization\n");
+    printf("5.communication and information in literacy skills\n");
+    printf("6.fundamentals of digital application and artificial intelligence\n");
+    printf("7.introduction to programming and algorithms\n");
+    printf("8.analogue electronic\n");
+    
+return 0;
+}   
+  
